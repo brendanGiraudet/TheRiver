1b1889f825b34da6debe64601c76308de111a47c
Brendan GIRAUDET <brendan.GIRAUDET@ebp.com>
2019-09-05 16:19:28 +0200

    more speed

--- main.cs
+++ main.cs
@@ -26,11 +26,15 @@ class Solution
 
         while (!r1List.Exists( o => o.Equals(val2)) && !r2List.Exists(o => o.Equals(val1)))
         {
-            val1 += SumOfDigit(val1);
+            //val1 += SumOfDigit(val1);
+            val1 += r1.Select(x => Convert.ToInt32(x.ToString())).Sum();
             r1List.Add(val1);
+            r1 = val1.ToString();
 
-            val2 += SumOfDigit(val2);
+            //val2 += SumOfDigit(val2);
+            val2 += r2.Select(x => Convert.ToInt32(x.ToString())).Sum();
             r2List.Add(val2);
+            r2 = val2.ToString();
         }
 
         var rep = r1List.Intersect(r2List).FirstOrDefault();
